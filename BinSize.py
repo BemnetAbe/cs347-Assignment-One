0af7faafe457ad9f8b4573d785309426c6f9f237
BemnetAbe <abebayehub@carleton.edu>
2023-09-13 10:21:28 -0500

    Create BinSize.py

---
+++ BinSize.py
@@ -0,0 +1,20 @@
+'''
+Each bin has a size of 100 units
+Different sizes of items within the same bin are seperated by "!" but each bin 
+is seperated by "#" 
+if the size of the items within one bin exceeds 100 it is invalid
+'''
+
+import sys
+import argparse
+import flask
+import json
+
+
+def newProblem():
+
+def placeItem():
+
+def endProblem():
+
+
