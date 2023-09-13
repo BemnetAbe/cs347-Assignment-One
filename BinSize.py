af79a3f4e0e370c8d997f14a6ad0e0bb113da3b7
Ben Zhao <zhaob@carleton.edu>
2023-09-13 11:00:28 -0500

    Co-authored-by: BemnetAbe <BemnetAbe@users.noreply.github.com>

--- BinSize.py
+++ BinSize.py
@@ -10,11 +10,51 @@ import argparse
 import flask
 import json
 
+app = flask.Flask(__name__)
 
+binPackingInstances = {}
+lastProblemID = 0
+
+def createNewProblem():
+    global lastProblemID
+    problemID = lastProblemID
+    problemID += 1
+    binEncoding = []
+    binPackingInstances[problemID] = binEncoding
+    return problemID, binEncoding
+
+@app.route('/')
+def main():
+    return 'Hello and Welcome to Bin Packing.'
+
+@app.route('/newproblem', methods=['GET'])
 def newProblem():
+    '''
+    Input: There is no input for this API endpoint
+    Output is a JSON Object: { 'ID': problemID 'bins' : binEncoding }
+    The problemID should be an integer that can be used to reference a particular set of bins that are being packed.
+    The binEncoding for a new (fresh) instance of bin packing should be an empty set containing no bins
+    '''
+
+    problemID, binEncoding = createNewProblem()
+
+    response = {
+        "ID": problemID, 'bins': binEncoding
+    }
+    return json.dumps(response)
+
+
 
 def placeItem():
+# The problemID should be the same as was provided in the input
 
+# new_item_size should be the size of the newly placed item
+
+# bin_number should be the number of the bin where the new item was placed. (Please number the first bin as 1)
+
+# The new_bin_encoding should be a string encoding the set of bins with the new itemed placed.
+
+# Multiple bins with its own ID that could have multiple bins that could be worked
 def endProblem():
 
 
